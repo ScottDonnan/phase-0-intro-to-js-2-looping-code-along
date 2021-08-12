ede329f362a3e5c5a392f3e9763a5e363787510a
Scott Donnan <scott.donnan@me.com>
2021-08-11 21:21:03 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards (array) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+return newArray;
+}
+writeCards(names);
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+countDown(5);
